764aba72d973deff0c104c91567075d5e023254d
Direncan Kaygisiz <direncan89@gmail.com>
2023-11-27 22:53:57 +0000

    almost complete,rails version error

---
+++ app/channels/chatroom_channel.rb
@@ -0,0 +1,12 @@
+class ChatroomChannel < ApplicationCable::Channel
+  def subscribed
+    chatroom = Chatroom.find(params[:id])
+    stream_for chatroom
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
+
+# Compare this snippet from app/javascript/channels/chatroom_channel.js:
